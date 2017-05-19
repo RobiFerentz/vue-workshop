6e85eeffd78bb1ce99cf3609dd56b7fa69524310
Robert Ferentz <RobiFerentz@gmail.com>
2017-05-19 09:28:56 +0300

    v1: starting point

---
+++ src/components/WordList.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello">
+    <ul>
+      <li class="inline" v-for="l in letters"><button>{{ l }}</button></li>
+    </ul>
+    <h1>Your list of {{ query }} words</h1>
+    <ul>
+      <li v-for="word in words">{{ word.word }}</li>
+    </ul>
+
+  </div>
+</template>
+
+<script type="text/babel">
+export default {
+  name: 'WordList',
+  data () {
+    return {
+      words: [],
+      query: 'V',
+      letters: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+    }
+  },
+  mounted() {
+    let apiKey = 'adc94b8adae78f1af5008067f2b03e31d8e17cfb54b0fef16'
+    let basePath = 'http://api.wordnik.com:80/v4/'
+    let {query} = this
+    let params = `?caseSensitive=false&minCorpusCount=5&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=2&maxLength=-1&skip=0&limit=20&api_key=${apiKey}`
+    let url = `${basePath}words.json/search/${query}${params}`
+    fetch(url)
+      .then(response=>response.json())
+      .then(json=>{
+        json.searchResults.shift()
+        this.words = json.searchResults
+      })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin: 0 10px;
+}
+
+li.inline {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import WordList from '@/components/WordList'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'WordList',
+      component: WordList
     }
   ]
 })
