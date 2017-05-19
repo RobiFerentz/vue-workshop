c8a5480279c3088493e50e71228ba81283513901
Robert Ferentz <RobiFerentz@gmail.com>
2017-05-19 07:23:19 +0300

    no comment

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -38,10 +38,10 @@ module.exports = {
         include: [resolve('src'), resolve('test')]
       },
       {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        test: /\.(png|jpe?g|gif|svg|ico)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
+          limit: 1,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
